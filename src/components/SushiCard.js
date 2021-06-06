cb33a84ae2484caf35c067847b6837c90c31b738
Cassandra <cparisi1290@gmail.com>
2021-06-06 18:34:52 -0500

    use props from sushi container to create individual sushi information for index

---
+++ src/components/SushiCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Container, Row, Col, Image } from 'react-bootstrap';
+
+const SushiCard = (props) => {
+    return (
+
+            <Container>
+                <Row>
+                    <Col xs={6} md={4}>
+                        <div key={props.sushi.id}>
+                            <Image className="sushi-index-image" src={props.sushi.image} alt={props.sushi.name} thumbnail/>                        </div>
+                            <Link to={`{/sushi/${props.sushi.name}`}><h4>{props.sushi.name}</h4></Link>
+                    </Col>
+                </Row>
+            </Container>
+ 
+    )
+}
+
+export default SushiCard
