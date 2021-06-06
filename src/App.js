ca91db27e1a847476a0a1c4db71ea25be66957fe
Cassandra <cparisi1290@gmail.com>
2021-06-06 09:14:22 -0500

    add navbar component and sushi, sashimi home routes from browser router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SushiContainer from './containers/sushiContainer'
-
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
+import Home from './components/Home';
+import sushiContainer from './containers/sushiContainer';
+import sashimiContainer from './containers/sashimiContainer';
 
 function App() {
   return (
     <div className="App">
-      <SushiContainer/>
+      <Router>
+        <NavBar/>
+        <div>
+          <Switch>
+            <Route path="/home" component={Home}></Route>
+            <Route path="/sushi" component={sushiContainer}></Route>
+            <Route path="/sashimi" component={sashimiContainer}></Route>
+          </Switch>
+        </div>
+
+
+      </Router>
+
     </div>
   );
 }
