478162c87b5a028727ea470b298e36e9716e6b72
Cassandra <cparisi1290@gmail.com>
2021-06-06 09:59:11 -0500

    set up createStore library with sushi and sahimi reducers and redux dev toolsf

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router } from 'react-router-dom'
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux'; 
+import sushiReducer from './reducers/sushiReducer';
+import sashimiReducer from './reducers/sashimiReducer';
+
+const rootReducer = combineReducers({
+  sushiReducer: sushiReducer,
+  sashimiReducer: sashimiReducer
+});
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
