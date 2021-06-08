5971180e8e7269c44e71ea2450ddd3a3fbd54a67
Cassandra <cparisi1290@gmail.com>
2021-06-08 17:04:38 -0500

    make containers names more explicit, add sushi show route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
+import { connect } from 'react-redux';
 import Home from './components/Home';
-import sushiContainer from './containers/sushiContainer';
-import sashimiContainer from './containers/sashimiContainer';
+import sushiContainer from './containers/sushiIndexContainer';
+import sashimiContainer from './containers/sashimiIndexContainer';
+import sushiShowContainer from './containers/sushiShowContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <NavBar/>
-        <div>
-          <Switch>
-            <Route path="/home" component={Home}></Route>
-            <Route path="/sushi" component={sushiContainer}></Route>
-            <Route path="/sashimi" component={sashimiContainer}></Route>
-          </Switch>
-        </div>
-
-
-      </Router>
-
-    </div>
-  );
+class App extends Component { 
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <NavBar/>
+          <div>
+            <Switch>
+              <Route path='/home' component={Home}></Route>
+              <Route path='/sushi' component={sushiContainer}></Route>
+              <Route path='/sashimi' component={sashimiContainer}></Route>
+              <Route path='/sushi/:id' component={sushiShowContainer}></Route>
+            </Switch>      
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App; 
+const mstp = state => {
+  return {
+    sushi: state.sushiReducer.sushi
+  }
+}
+export default connect(mstp)(App); 
