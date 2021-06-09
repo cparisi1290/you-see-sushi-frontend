2f4e7f886e75f33083efad03b06ce7e33acb2a6f
Cassandra <cparisi1290@gmail.com>
2021-06-09 05:58:25 -0500

    organize files and folders

--- src/components/SashimiIndex.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import SashimiIndexCard from './SashimiIndexCard'
-// import { Container, Row, Col } from 'react-bootstrap';
-
-export default function SashimiIndex(props) {
-    // console.log(props)
-    const sashimi = props.sashimi.map(sashimi => <div key={sashimi.id}><SashimiIndexCard sashimi={sashimi}/></div>)
-    return (sashimi)
-}
