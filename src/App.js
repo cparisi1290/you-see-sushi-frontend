a751b184ac39b484fe9523a99ae5e0699456e502
Cassandra <cparisi1290@gmail.com>
2021-06-09 05:53:52 -0500

    remove store and sushi show route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar';
+import NavBar from './common-components/NavBar';
 import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
 import { connect } from 'react-redux';
-import Home from './components/Home';
+import Home from './common-components/Home';
 import sushiContainer from './containers/sushiIndexContainer';
 import sashimiContainer from './containers/sashimiIndexContainer';
-import sushiShowContainer from './containers/sushiShowContainer'
 
 class App extends Component { 
   render() {
@@ -20,18 +19,12 @@ class App extends Component {
               <Route path='/home' component={Home}></Route>
               <Route path='/sushi' component={sushiContainer}></Route>
               <Route path='/sashimi' component={sashimiContainer}></Route>
-              <Route path='/sushi/:id' component={sushiShowContainer}></Route>
             </Switch>      
           </div>
         </Router>
-      </div>
+        </div>
     );
   }
 }
 
-const mstp = state => {
-  return {
-    sushi: state.sushiReducer.sushi
-  }
-}
-export default connect(mstp)(App); 
+export default App; 
