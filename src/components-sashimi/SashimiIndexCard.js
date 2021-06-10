4b6fb5ce95df58797dab9b4652a8c98aa365cdac
Cassandra <cparisi1290@gmail.com>
2021-06-10 10:38:39 -0500

    add jsx for styling

--- src/components-sashimi/SashimiIndexCard.js
+++ src/components-sashimi/SashimiIndexCard.js
@@ -1,17 +1,30 @@
 import React from 'react'
-import { Image } from 'react-bootstrap'
+import { Image, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 export default function SashimiIndexCard(props) {
     // console.log(props)
     return (
-        <div>
-            <Link to={`/sashimi/${props.sashimi.id}`}>
-                <Image className="sashimi-index-image" src={props.sashimi.image} alt={props.sashimi.name} thumbnail/>
-            </Link>
-            <Link to={`/sashimi/${props.sashimi.id}`}>
-                <h4>{props.sashimi.name}   |    ${props.sashimi.price}</h4>
-            </Link>
+        <div key={props.sashimi.id} className='sashimi-index-card'>
+            <div className="sashimi-index-image">
+                <Link to={`/sashimi/${props.sashimi.id}`}>
+                    <Image  src={props.sashimi.image} alt={props.sashimi.name} thumbnail/>
+                </Link>
+            </div>
+            <div className="sashimi-name">
+                <Link className='sashimi-links' to={`/sashimi/${props.sashimi.id}`}>
+                    <h4>{props.sashimi.name}</h4>
+                </Link>
+                    
+                
+            </div>
+            <div className="sashimi-price">
+                <span>${props.sashimi.price}</span>
+                
+            </div>
+            <div className="sashimi-index-order-button">
+            <Button variant="success">Add to Order</Button>
+            </div>
         </div>
     )
 }
