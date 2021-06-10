5a627e98e6d4b1ccd8c6f1ba39de970df5dd3c45
Cassandra <cparisi1290@gmail.com>
2021-06-10 10:40:52 -0500

    add jsx for styling

--- src/components-sushi/SushiShow.js
+++ src/components-sushi/SushiShow.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image } from 'react-bootstrap'
+import { Card, Button, Container, Row, Col } from 'react-bootstrap'
 
 const SushiShow = (props) => {
     
@@ -8,11 +8,21 @@ const SushiShow = (props) => {
     })
 
     return (
-        <div>
-            <Image src={sushi.image} alt={sushi.name}></Image>
-            <h3>{sushi.name}    |    ${sushi.price}</h3>
-            <p>{sushi.description}</p>
-        </div>
+        <Container className='sushi-show-container'>
+            <Row>
+                <Col>
+                    <Card style={{ width: '30rem' }} fluid sm>
+                        <Card.Img variant="top" src={sushi.image} alt={sushi.name} />
+                        <Card.Body>
+                            <Card.Title>{sushi.name}</Card.Title>
+                            <Card.Text>${sushi.price}</Card.Text>
+                            <Card.Text>{sushi.description}</Card.Text>
+                            <Button variant="success">Add to Order</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
