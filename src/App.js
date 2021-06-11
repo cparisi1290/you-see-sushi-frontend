b6723f98200648f723bc13e2cf80c7e6ecc3aa25
Cassandra <cparisi1290@gmail.com>
2021-06-11 17:15:46 -0500

    add useEffect and useDispatch hooks to give app component and searchbar component access to store

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './common-components/NavBar';
-import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
-import { connect } from 'react-redux';
+import { Switch, Route, } from "react-router-dom";
+import { useDispatch } from 'react-redux';
 import Home from './common-components/Home';
 import sushiContainer from './containers/sushiIndexContainer';
 import sashimiContainer from './containers/sashimiIndexContainer';
+import orderContainer from './containers/orderContainer';
+import { fetchSushi } from './actions/sushiActions'
+import { fetchSashimi } from './actions/sashimiActions'
 
-class App extends Component { 
-  render() {
+
+const App = () => { 
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchSushi())
+    dispatch(fetchSashimi())
+  }, [])
+
     return (
       <div className="App">
-        <Router>
-          <NavBar/>
-          <div>
-            <Switch>
-              <Route exact path='/' component={Home}></Route>
-              <Route path='/home' component={Home}></Route>
-              <Route path='/sushi' component={sushiContainer}></Route>
-              <Route path='/sashimi' component={sashimiContainer}></Route>
-            </Switch>      
-          </div>
-        </Router>
+        <NavBar/>
+        <div>
+          <Switch>
+            <Route exact path='/' component={Home}></Route>
+            <Route path='/home' component={Home}></Route>
+            <Route path='/sushi' component={sushiContainer}></Route>
+            <Route path='/sashimi' component={sashimiContainer}></Route>
+          </Switch>      
         </div>
+      </div>
     );
   }
-}
 
 export default App; 
