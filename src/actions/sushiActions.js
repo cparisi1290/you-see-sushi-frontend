f320b2bcd16ae74d437ed0edf332a1f99990a613
Cassandra <cparisi1290@gmail.com>
2021-06-11 17:17:18 -0500

    console log to confirm data coming through

--- src/actions/sushiActions.js
+++ src/actions/sushiActions.js
@@ -1,11 +1,14 @@
 // ASYNC ACTION CREATORS
 
+import SushiIndexCard from "../components-sushi/SushiIndexCard"
+
 export const fetchSushi = () => {
     return(dispatch) =>{
         console.log('success')
         fetch('http://localhost:3001/api/v1/sushi_rolls')
         .then(resp => resp.json())
         .then(sushi => {
+            // console.log(sushi)
             if (sushi.error) {
                 alert(sushi.error)
             } else {
