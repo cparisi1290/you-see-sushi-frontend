65b34ae3af05168f6a64b83d8502275afd4cc06b
Cassandra <cparisi1290@gmail.com>
2021-06-11 17:23:14 -0500

    add case statement for filtered sushi to return that item when searched for

--- src/reducers/sushiReducer.js
+++ src/reducers/sushiReducer.js
@@ -1,10 +1,12 @@
-const sushiReducer = (state = {sushi: [], loading: false}, action) => {
+const sushiReducer = (state = {sushi: [], filteredSushi: [], loading: false}, action) => {
     switch (action.type) {
         case 'FETCH_SUSHI':
             return {
                 ...state, 
                 sushi: action.payload, // array of objects that came back from fetch request
                 loading: false}
+                case 'SEARCH_SUSHI':
+        return ({...state, filteredSushi: action.payload})
         case 'LOADING':
             return {...state, loading: true}
         default:
